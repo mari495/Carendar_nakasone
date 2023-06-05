83593b4521762609ac021f2fbd0fad75e27cbb33
mari495 <somari3915@gmail.com>
2023-06-06 05:57:00 +0900

    ラベル修正

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -10,8 +10,7 @@ import javafx.scene.control.Label;
 
 
 public class Controller {
-	//、31個のLabelオブジェクトをリストとして保持
-	private List<Label> dateLabels = new ArrayList<>();
+	
 
 	
 	@FXML private Label dateLabel1;
@@ -60,8 +59,10 @@ public class Controller {
 	@FXML private Label dateLabel34;
 	@FXML private Label dateLabel35;
 	
-	
-	@FXML
+	//、31個のLabelオブジェクトをリストとして保持
+		private List<Label> dateLabels = new ArrayList<>();
+		
+	@FXML//ラベルをリストに追加
 	public void initialize() {
 	    dateLabels.add(dateLabel1);
 	    dateLabels.add(dateLabel2);
@@ -101,26 +102,28 @@ public class Controller {
 	    dateLabels.add(dateLabel33);
 	    dateLabels.add(dateLabel34);
 	    dateLabels.add(dateLabel35);
+	    //currentDate を現在の日付で初期化withMonth(1).withDayOfMonth(1) の部分で月を1月、日を1日に設定
 	    LocalDate currentDate = LocalDate.now().withMonth(1).withDayOfMonth(1);
 
 	    for (int i = 0; i < 31; i++) {
-	        int dayOfMonth = currentDate.getDayOfMonth();
-	        dateLabels.get(i).setText(String.valueOf(dayOfMonth));
+	        int dayOfMonth = currentDate.getDayOfMonth();//currentDate の日付を取得
+	        
+	        dateLabels.get(i).setText(String.valueOf(dayOfMonth));//対応するラベルに日付を設定
 
-	        currentDate = currentDate.plusDays(1);
+	        currentDate = currentDate.plusDays(1);//currentDate を1日進める
 	    }
 
-	    currentDate = currentDate.withMonth(2).withDayOfMonth(1);
+	    currentDate = currentDate.withMonth(2).withDayOfMonth(1);//currentDate を2月1日に設定
 
 	    for (int i = 31; i < 35; i++) {
-	        if (currentDate.getMonthValue() == 2) {
-	            int dayOfMonth = currentDate.getDayOfMonth();
-	            dateLabels.get(i).setText(String.valueOf(dayOfMonth));
+	        if (currentDate.getMonthValue() == 2) {//現在の月が2月であるかをチェック
+	            int dayOfMonth = currentDate.getDayOfMonth();//currentDate の日付部分の値を取得してdayOfMonthに代入
+	            dateLabels.get(i).setText(String.valueOf(dayOfMonth));//dayOfMonth の値を文字列に変換してラベルのテキストとして設定
 	        } else {
-	            dateLabels.get(i).setText("");
+	            dateLabels.get(i).setText("");//該当するラベルのテキストを空に設定
 	        }
 
-	        currentDate = currentDate.plusDays(1);
+	        currentDate = currentDate.plusDays(1);//currentDate を1日進める、これにより、次の日付を設定するための準備
 	    }
 	}
 }
